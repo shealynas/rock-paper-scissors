53d309f3cbab8cbed5d4f23b1f8c7b187fe00bef
Shealyn Sugai <shealyn@student.dev-island.org>
2023-06-19 22:10:46 -1000

    got playRound to function

--- index.js
+++ index.js
@@ -25,64 +25,75 @@ alert(`Hey ${userName}! Let's play! Best out of 5 rounds wins!`);*/
 
 let playerScore = 0;
 let computerScore = 0;
-let roundWinner = 0;
+let roundCount = 0;
 //should it be "" or is it 0 because let changes
 
-//Player Chooses Option
-let playerChoice = prompt(
-  "Choose your weapon: rock, paper, or scissors"
-).toLowerCase();
+while (roundCount < 5) {
+  //Player Chooses Option
+  let playerChoice = prompt(
+    "Choose your weapon: rock, paper, or scissors"
+  ).toLowerCase();
 
-while (
-  playerChoice !== "rock" &&
-  playerChoice !== "paper" &&
-  playerChoice !== "scissors"
-) {
-  playerChoice = prompt("Choose your weapon: rock, paper, or scissors");
-}
-console.log(playerChoice);
+  while (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    playerChoice = prompt("Choose your weapon: rock, paper, or scissors");
+  }
+  console.log(playerChoice);
 
-//computer random choice
-function computerChoice() {
+  //computer random choice
   let computerMath = Math.floor(Math.random() * 3);
   console.log(computerMath);
 
-  //let computerChoice = () => {
+  let computerChoice;
   if (computerMath === 0) {
-    return "rock";
+    computerChoice = "rock";
   } else if (computerMath === 1) {
-    return "paper";
+    computerChoice = "paper";
   } else {
-    return "scissors";
+    computerChoice = "scissors";
   }
-}
 
-console.log(computerChoice());
+  let playRound = () => {
+    console.log(playerChoice, computerChoice);
+    if (playerChoice === computerChoice) {
+      return alert("Tie!");
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "scissors" && computerChoice === "paper") ||
+      (playerChoice === "paper" && computerChoice === "rock")
+    ) {
+      playerScore++;
+      return alert(`You Win! ${playerChoice} beats ${computerChoice}`);
+    } else {
+      computerScore++;
+      return alert(
+        `You Lose! ${playerChoice} is defeated by ${computerChoice}`
+      );
 
-//should this be a variable
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return alert("Tie!");
-    //do i need to add ++ to both
-  } else if (playerChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    return alert("Computer Wins! Paper beats Rock.");
-  } else if (playerChoice === "rock" && computerChoice === "scissors") {
-    playerScore++;
-    return alert("You Win! Rock beats Scissors!");
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
-    playerScore++;
-    return alert("You Win! Paper beats Rock!");
-  } else if (playerChoice === "paper" && computerChoice === "scissors") {
-    computerScore++;
-    return alert("Computer Wins! Scissors beats Paper!");
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
-    playerScore++;
-    return alert("You Win! Scissors beats Paper!");
-  } else if (playerChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    return alert("Computer Wins! Rock beats Scissors!");
-    //can i return an alert like this
-  }
+      /*} else if (playerChoice === "rock" && computerChoice === "paper") {
+      computerScore++;
+      return alert("Computer Wins! Paper beats Rock.");
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+      playerScore++;
+      return alert("You Win! Rock beats Scissors!");
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+      playerScore++;
+      return alert("You Win! Paper beats Rock!");
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+      computerScore++;
+      return alert("Computer Wins! Scissors beats Paper!");
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+      playerScore++;
+      return alert("You Win! Scissors beats Paper!");
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+      computerScore++;
+      return alert("Computer Wins! Rock beats Scissors!");*/
+    }
+  };
+  console.log(playRound());
+
+  roundCount++;
 }
-console.log(playRound());
