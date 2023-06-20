db8a8f777038c244d211986f8b3d39057e3334dc
Shealyn Sugai <shealyn@student.dev-island.org>
2023-06-20 07:42:56 -1000

    Completed Game

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 //best out of 5 rounds
 //game winner
 
-/*Beginning Prompt
+//Beginning Prompt
 function promptUserForName() {
   const WELCOME_MESSAGE =
     "Welcome to Rock Paper Scissors! Please enter your name to play";
@@ -21,13 +21,13 @@ function promptUserForName() {
 
 const userName = promptUserForName();
 
-alert(`Hey ${userName}! Let's play! Best out of 5 rounds wins!`);*/
+alert(`Hey ${userName}! Let's play! Best out of 5 rounds wins!`);
 
 let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
-//should it be "" or is it 0 because let changes
 
+//Best of 5 rounds loop
 while (roundCount < 5) {
   //Player Chooses Option
   let playerChoice = prompt(
@@ -43,7 +43,7 @@ while (roundCount < 5) {
   }
   console.log(playerChoice);
 
-  //computer random choice
+  //Computer random choice
   let computerMath = Math.floor(Math.random() * 3);
   console.log(computerMath);
 
@@ -56,6 +56,7 @@ while (roundCount < 5) {
     computerChoice = "scissors";
   }
 
+  //Winner prompt
   let playRound = () => {
     console.log(playerChoice, computerChoice);
     if (playerChoice === computerChoice) {
@@ -72,28 +73,21 @@ while (roundCount < 5) {
       return alert(
         `You Lose! ${playerChoice} is defeated by ${computerChoice}`
       );
-
-      /*} else if (playerChoice === "rock" && computerChoice === "paper") {
-      computerScore++;
-      return alert("Computer Wins! Paper beats Rock.");
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-      playerScore++;
-      return alert("You Win! Rock beats Scissors!");
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-      playerScore++;
-      return alert("You Win! Paper beats Rock!");
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-      computerScore++;
-      return alert("Computer Wins! Scissors beats Paper!");
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-      playerScore++;
-      return alert("You Win! Scissors beats Paper!");
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-      computerScore++;
-      return alert("Computer Wins! Rock beats Scissors!");*/
     }
   };
   console.log(playRound());
 
   roundCount++;
 }
+
+//Game winner
+let endGame = () => {
+  if (playerScore > computerScore) {
+    return alert("Game Over. YOU WON!");
+  } else if (computerScore > playerScore) {
+    return alert("Game Over. Sorry you lose.");
+  } else {
+    return alert("Game Over. You Tied!");
+  }
+};
+console.log(endGame());
